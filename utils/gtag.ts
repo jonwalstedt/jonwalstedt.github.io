4337e20df84b339f2b9af59a3905128d4b6870f3
Jon Wålstedt <jon.walstedt@king.com>
2020-11-06 22:42:22 +0100

    Use new ga tracking id

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -1,4 +1,5 @@
-export const GA_TRACKING_ID = 'UA-21627223-2'; // This is your GA Tracking ID
+// export const GA_TRACKING_ID = 'UA-21627223-2'; // This is your GA Tracking ID
+export const GA_TRACKING_ID = 'G-955LH5V4TV'; // This is your GA Tracking ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
